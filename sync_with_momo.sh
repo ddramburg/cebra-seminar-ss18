0f0d116246e4099aec1321caf8e0ee399a58ac26
Sergio Siccha <sergio.siccha@gmail.com>
2018-02-16 11:42:54 +0100

    Improve sync

--- sync_with_momo.sh
+++ sync_with_momo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # a = archive, z = zip, P = show Progress
 rsync -azP Teilnehmer.ods momo:~/lehre/seminare/cebra-seminar-ss18/
+rsync -azP literatur momo:~/lehre/seminare/cebra-seminar-ss18/
