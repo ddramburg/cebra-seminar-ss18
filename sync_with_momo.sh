650c9e03dcc3d59ddb03ef79ff57744b73e04155
Sergio Siccha <sergio.siccha@gmail.com>
2018-02-26 11:55:05 +0100

    Improve sync

--- sync_with_momo.sh
+++ sync_with_momo.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# a = archive, z = zip, P = show Progress
+# rsync options: a = archive, z = zip, P = show Progress
+# first get updates from the remote
+rsync -azP momo:~/lehre/seminare/cebra-seminar-ss18/Teilnehmer.ods Teilnehmer.ods
+rsync -azP momo:~/lehre/seminare/cebra-seminar-ss18/literatur literatur
+# now push updates to the remote
 rsync -azP Teilnehmer.ods momo:~/lehre/seminare/cebra-seminar-ss18/
 rsync -azP literatur momo:~/lehre/seminare/cebra-seminar-ss18/
