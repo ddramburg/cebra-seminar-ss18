88094e7e1a786fae2c963e4a80b318d7568ffdc2
Sergio Siccha <sergio.siccha@gmail.com>
2018-04-11 13:40:24 +0200

    Add folder ausarbeitungen/

--- sync_with_momo.sh
+++ sync_with_momo.sh
@@ -3,6 +3,8 @@
 # first get updates from the remote
 rsync -auzP momo:~/lehre/seminare/cebra-seminar-ss18/Teilnehmer.ods Teilnehmer.ods
 rsync -auzP momo:~/lehre/seminare/cebra-seminar-ss18/literatur literatur
+rsync -auzP momo:~/lehre/seminare/cebra-seminar-ss18/ausarbeitungen ausarbeitungen
 # now push updates to the remote
 rsync -auzP Teilnehmer.ods momo:~/lehre/seminare/cebra-seminar-ss18/
 rsync -auzP literatur momo:~/lehre/seminare/cebra-seminar-ss18/
+rsync -auzP ausarbeitungen momo:~/lehre/seminare/cebra-seminar-ss18/
